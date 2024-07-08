9ac2db0aa4b5724570c8d59a1fe606f060c87fcd
git-hariom <hariom.sinha.dev@gmail.com>
2024-07-08 16:06:31 +0530

    Hari | Test changes

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,9 @@ import ButtonV3 from "./library/v3/components/Button";
 function App() {
   return (
     <div className="App">
-      <ButtonV1>Button V1</ButtonV1>
-      <ButtonV2>Button V2</ButtonV2>
-      <ButtonV3>Button V3</ButtonV3>
+      <ButtonV1>Button V1 test</ButtonV1>
+      <ButtonV2>Button V2 test</ButtonV2>
+      <ButtonV3>Button V3 test</ButtonV3>
     </div>
   );
 }
